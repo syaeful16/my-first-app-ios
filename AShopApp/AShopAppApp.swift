f82955fa2d545bfa50fc0e847bb6fc9a71a14463
Syaefulloh Arnas <syaefulanas121@gmail.com>
2024-06-25 09:35:44 +0700

    Initial Commit

---
+++ AShopApp/AShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AShopAppApp.swift
+//  AShopApp
+//
+//  Created by Syaefulloh Arnas on 25/06/24.
+//
+
+import SwiftUI
+
+@main
+struct AShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AShopApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AShopApp
+//
+//  Created by Syaefulloh Arnas on 25/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
